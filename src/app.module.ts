f82737d244c91aa7f4459aac541a32ee22678746
totozunino <mathiaszunino@gmail.com>
2022-05-08 16:30:32 -0300

    feat: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigurationModule } from './config/config.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
+    ConfigurationModule,
     PostsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'docker',
-      database: 'blog',
-      autoLoadEntities: true,
-      synchronize: process.env.NODE_ENV !== 'production',
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PSQL_HOST'),
+        port: configService.get<number>('PSQL_PORT'),
+        username: configService.get('PSQL_USERNAME'),
+        password: configService.get('PSQL_PASSWORD'),
+        database: configService.get('PSQL_DB'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
